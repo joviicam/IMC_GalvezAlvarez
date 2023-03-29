b33ef088fec5cbb27aad4871fd1ce350b478d807
Unknow <20213tn102@utez.edu.mx>
2023-03-29 14:40:22 -0600

    primeras pantallas

---
+++ src/screens/DataScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function DataScreen() {
+  return (
+    <view>
+        <text>ROL:     </text>
+        <text> Hola , tienes años, y tu IMC es: , estas clasificado en</text>
+
+        <button>SALIR</button>
+        
+    </view>
+  )
+}
---
+++ src/screens/FormScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function FormScreen() {
+  return (
+    <view>
+        
+
+        <input placeholder='Peso'></input>
+
+        <input placeholder='Altura'></input>
+
+        <input placeholder='año de nacimiento'></input>
+
+        <button>Calcular IMC</button>
+    </view>
+  )
+}
